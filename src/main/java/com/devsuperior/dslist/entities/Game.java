fe7552334161e67cfa740e717dd5d345bfb1489d
PARATELLI <pedro.p.paratelli@accenture.com>
2023-05-09 15:48:22 -0300

    Game Seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -19,7 +19,11 @@ public class Game {
     private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")//forçando os campos a serem tipo texto no BD//
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
