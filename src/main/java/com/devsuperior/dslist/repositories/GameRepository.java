7272d51ef5faaf65f4480b7a5e52c29cc6ce68e1
PARATELLI <pedro.p.paratelli@accenture.com>
2023-05-10 18:38:15 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -3,7 +3,7 @@ package com.devsuperior.dslist.repositories;
 import com.devsuperior.dslist.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//Camada de Acesso a Dados - Componente que faz consulta com BD, atualiza e todas as...//
+//Camada de Acesso a Dados - Componente que faz consulta no BD, atualiza e todas as...//
 //outras operações graças a configurações ja feitas dentro do JpaRepository//
 public interface GameRepository extends JpaRepository<Game, Long> {
 }
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dslist.services;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +18,13 @@ public class GameService {
     @Autowired//injetando uma instancia do GameRepository dentro do GameService//
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)//Assegurando que nao serão feitas operações de escrita no banco de dados, melhora performance//
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         //transformou a lista de "Games" numa lista "GameMinDTO"//
