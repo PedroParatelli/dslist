0acec75928241005cf1ff83d0b4c7f8a7758b484
PARATELLI <pedro.p.paratelli@accenture.com>
2023-05-10 19:40:34 -0300

    SQL search, projections

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -1,9 +1,25 @@
 package com.devsuperior.dslist.repositories;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 //Camada de Acesso a Dados - Componente que faz consulta no BD, atualiza e todas as...//
 //outras operações graças a configurações ja feitas dentro do JpaRepository//
 public interface GameRepository extends JpaRepository<Game, Long> {
+
+    //Repository que acessa o banco de dados, as consultas SQL são expressas no repository//
+    @Query(nativeQuery = true, value = """
+		SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+		tb_game.short_description AS shortDescription, tb_belonging.position
+		FROM tb_game
+		INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
+		WHERE tb_belonging.list_id = :listId
+		ORDER BY tb_belonging.position
+			""")
+    List<GameMinProjection> searchByList(Long listId);
+
 }
